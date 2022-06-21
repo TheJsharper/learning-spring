d838d365998068e7ec128c8807f6a518f937e098
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-06-22 00:14:40 +0200

    trying to fix problem

--- src/main/java/com/learningspringboot/learningspring/utils/AppStartupEvent.java
+++ src/main/java/com/learningspringboot/learningspring/utils/AppStartupEvent.java
@@ -2,12 +2,15 @@ package com.learningspringboot.learningspring.utils;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.learningspringboot.learningspringa.api.RoomRepository;
 
 @Component
-public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+@Order(0)
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent>{
 	private final RoomRepository roomRepository;
 	
 	public AppStartupEvent(RoomRepository roomRepository) {
@@ -18,6 +21,7 @@ public class AppStartupEvent implements ApplicationListener<ApplicationReadyEven
 	}
 	
 	@Override
+	//@EventListener(ApplicationReadyEvent.class)
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		System.out.println("======");
 		this.roomRepository.findAll().forEach(System.out::println);
