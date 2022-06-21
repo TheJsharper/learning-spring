9000f94348a3c9463872727e0c5163da34d92eb1
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-06-22 00:44:06 +0200

    trying to fix problem

--- src/main/java/com/learningspringboot/learningspring/LearningSpringApplication.java
+++ src/main/java/com/learningspringboot/learningspring/LearningSpringApplication.java
@@ -3,12 +3,17 @@ package com.learningspringboot.learningspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.learningspringboot.learningspring.utils.AppStartupEvent;
+
 @SpringBootApplication (scanBasePackages =   {"com.learningspringboot.learningspring.*.*"})
 public class LearningSpringApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(LearningSpringApplication.class, args);
+		/*;*/var context =new SpringApplication(LearningSpringApplication.class);
+		context.addListeners(new AppStartupEvent());
+		context.run(args);
+		//SpringApplication.run(LearningSpringApplication.class, args);
 	}
 
 }
--- src/main/java/com/learningspringboot/learningspring/utils/AppStartupEvent.java
+++ src/main/java/com/learningspringboot/learningspring/utils/AppStartupEvent.java
@@ -1,30 +1,41 @@
 package com.learningspringboot.learningspring.utils;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import com.learningspringboot.learningspring.data.Room;
 import com.learningspringboot.learningspringa.api.RoomRepository;
 
 @Component
 @Order(0)
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent>{
-	private final RoomRepository roomRepository;
+	@Autowired
+	private  RoomRepository roomRepository;
+	private Iterable<Room> iterator;
 	
-	public AppStartupEvent(RoomRepository roomRepository) {
-		this.roomRepository = roomRepository;
+	/**/public AppStartupEvent(/*RoomRepository roomRepository*/) {
+		//this.roomRepository = roomRepository;
 		System.out.println("Construct---------------------");
 				
+		//this.iterator =this.roomRepository.findAll();
 		
 	}
+	@PostConstruct
+    public void init() {
+       // LOG.info(Arrays.asList(environment.getDefaultProfiles()));
+		System.out.println("======");
+    }
 	
 	@Override
 	//@EventListener(ApplicationReadyEvent.class)
 	public void onApplicationEvent(ApplicationReadyEvent event) {
-		System.out.println("======");
-		this.roomRepository.findAll().forEach(System.out::println);
+		
+		//this.iterator.forEach(System.out::println);
 		
 	}
 
